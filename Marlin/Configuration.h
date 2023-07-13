af4c35ac0b75beb95289d21e4778c3b14467fd1e
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2023-07-13 10:55:14 +0100

    - Updated probe z offset value. - Reduced the grid point count to 3 for probing.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1576,7 +1576,7 @@
  */
 // #define NOZZLE_TO_PROBE_OFFSET { -42.50, -18.80, -0.840 }  // Direct Extruder + CR-Touch
 // #define NOZZLE_TO_PROBE_OFFSET { -25, -42, -2.670 }  // Sprite Extruder Pro + CR-Touch
-#define NOZZLE_TO_PROBE_OFFSET { -25.000, -42.000, -2.450 }  // Sprite Extruder Pro + CR-Touch
+#define NOZZLE_TO_PROBE_OFFSET { -25.000, -42.000, -3.200 }  // Sprite Extruder Pro + CR-Touch
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
@@ -2099,7 +2099,7 @@
 #if ANY(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 7
+  #define GRID_MAX_POINTS_X 3
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
