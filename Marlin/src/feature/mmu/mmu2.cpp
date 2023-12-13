05f108d2a7523ffac9c45c2db7682f9c3ff7115c
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2023-12-13 10:14:42 +0000

    mmu2:
    Fix the excesive extrusion problem when MMU is commanded to run "C0".

--- Marlin/src/feature/mmu/mmu2.cpp
+++ Marlin/src/feature/mmu/mmu2.cpp
@@ -880,11 +880,14 @@ void MMU2::filament_runout() {
       if (present && !mmu2s_triggered) {
         DEBUG_ECHOLNPGM("MMU <= 'A'");
         MMU2_SEND("A");
-      }
-      // Slowly spin the extruder during C0
-      else {
-        while (planner.movesplanned() < 3)
-          unscaled_mmu2_e_move(0.25, MMM_TO_MMS(120), false);
+      } else {
+        while (planner.movesplanned() < 3) {
+          // do nothing here as the previous implementation of
+          // "keep slowly spinning during C0" causes a huge amount of
+          // filament to be extruded and causing catastrophic/unrecoverable
+          // issues...
+          idle();
+        }
       }
     }
     mmu2s_triggered = present;
